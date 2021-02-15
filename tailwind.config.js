0dec3dd3fb66304b33b695dfc3b54a369031ab3b
Muguku <hello@muguku.co.ke>
2021-02-15 04:04:21 +0300

    remove unused styles in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
+// tailwind.config.js
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
